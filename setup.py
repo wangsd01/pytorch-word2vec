0a87626601096ee6ac0c76aa6131ac7acc05b18e
wangsd01 <kun.wang2012@rutgers.edu>
2018-03-20 17:14:16 -0400

    fix bug in compile setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from distutils.extension import Extension
 
-setup(ext_modules = cythonize('data_producer.pyx'))
+ext_modules = [
+    Extension(
+	name='data_producer._mask',
+        sources=['data_producer.pyx'],
+	extra_link_args=['-L/usr/lib/x86_64-linux-gnu/'],
+    )
+]
+
+setup(ext_modules = cythonize(ext_modules))
+
